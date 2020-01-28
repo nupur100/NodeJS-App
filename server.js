44e29a65eb93ba0ce04cdeb68726cf1620ba530d
nupur100 <37748784+nupur100@users.noreply.github.com>
2020-01-28 12:33:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {    res.send('Hello world from a Node.js app. This is CI/CD of Node JS App!')})
+app.get('/', (req, res) => {    res.send('Hello world from a Node.js app. This is CI/CD of Node JS App! Updation')})
 app.listen(3000, () => {    console.log('Server is up on 3000')})
